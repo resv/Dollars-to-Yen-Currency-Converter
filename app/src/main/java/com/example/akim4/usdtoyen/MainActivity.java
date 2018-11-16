3ee7a4afe61c637aa24fd477ae03c23d62e72e10
Adam Kim <akim456@gmail.com>
2018-11-15 21:04:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/akim4/usdtoyen/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.akim4.usdtoyen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertUsd(View view){
+
+    EditText editText = (EditText) findViewById(R.id.usdAmount);
+
+    String amountInDollars = editText.getText().toString();
+
+    double amountInDollarsDouble = Double.parseDouble(amountInDollars);
+
+    double amountInYenDouble = amountInDollarsDouble * 113.45;
+
+    String amountInYenString = String.format("%.2f", amountInYenDouble);
+
+        Toast.makeText(this, "$" + amountInDollars + " Dollars equals to ¥" + amountInYenString + " Yen", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public void convertYen(View view){
+
+        EditText  editText = (EditText) findViewById(R.id.yenAmount);
+
+        String amountInYen = editText.getText().toString();
+
+        double amountInYenDouble = Double.parseDouble(amountInYen);
+
+        double amountInDollarsDouble = amountInYenDouble * 0.0088;
+
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
+
+        Toast.makeText(this, "¥" + amountInYen + " Yen equals to $" + amountInDollarsString + " Dollars", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
