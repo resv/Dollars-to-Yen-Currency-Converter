ec3621437f9e67f11a4741b40268444147622653
Adam Kim <akim456@gmail.com>
2018-11-24 23:56:26 -0500

    added rate and share buttons to app info and they both work

--- app/src/main/java/com/myapp/akim4/usdtoyen/AppInfoActivity.java
+++ app/src/main/java/com/myapp/akim4/usdtoyen/AppInfoActivity.java
@@ -1,11 +1,23 @@
 package com.myapp.akim4.usdtoyen;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+
+import static java.util.jar.Pack200.Packer.ERROR;
 
 public class AppInfoActivity extends AppCompatActivity {
 
@@ -15,6 +27,39 @@ public class AppInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info);
+
+        //RATE APP BUTTON
+            Button rateAppButton = (Button) findViewById(R.id.rateAppButton);
+            rateAppButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Uri uri = Uri.parse("market://details?id=" + getPackageName());
+                    Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                    goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                            Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                            Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                    try {
+                        startActivity(goToMarket);
+                    } catch (ActivityNotFoundException e) {
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                    }
+                }
+            });
+
+        //SHARE APP BUTTON
+            Button shareAppButton = (Button) findViewById(R.id.shareAppButton);
+            shareAppButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                    sharingIntent.setType("text/plain");
+                    String shareBody = "Here is the share content body";
+                    sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                    sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                    startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                }
+            });
 
         //---------------------------------------------------------------//
         //AD INFO
