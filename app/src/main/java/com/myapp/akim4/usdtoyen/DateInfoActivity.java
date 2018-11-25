881b38bf55d5538d100fbb68ce627c04199b3f2d
Adam Kim <akim456@gmail.com>
2018-11-24 21:43:28 -0500

    added dateInfoActivity content, added ads to all activities

--- app/src/main/java/com/myapp/akim4/usdtoyen/DateInfoActivity.java
+++ app/src/main/java/com/myapp/akim4/usdtoyen/DateInfoActivity.java
@@ -3,11 +3,26 @@ package com.myapp.akim4.usdtoyen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class DateInfoActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_info);
+
+        //---------------------------------------------------------------//
+        //AD INFO
+
+        // ADBMOD INSTANTIATION
+        MobileAds.initialize(this, "ca-app-pub-9665161606825012/3253543710");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
