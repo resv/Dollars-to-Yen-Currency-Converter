a46461cb8feeb13879b4aad1c7de09a80147b27c
Adam Kim <akim456@gmail.com>
2018-11-25 22:13:16 -0500

    added contact dev button and function with content

--- app/src/main/java/com/myapp/akim4/usdtoyen/AppInfoActivity.java
+++ app/src/main/java/com/myapp/akim4/usdtoyen/AppInfoActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -22,6 +23,8 @@ import static java.util.jar.Pack200.Packer.ERROR;
 public class AppInfoActivity extends AppCompatActivity {
 
     private AdView mAdView;
+    int versionCode = BuildConfig.VERSION_CODE;
+    String versionName = BuildConfig.VERSION_NAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +61,24 @@ public class AppInfoActivity extends AppCompatActivity {
                 sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Check out this app that converts Dollars and Yen!");
                 sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
                 startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
+
+        //CONTACT DEV BUTTON
+        Button contactDevButton = (Button) findViewById(R.id.contactDevButton);
+        contactDevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"Android@AtomKim.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "APPLICATION : DOLLARS TO YEN, (Version Code: " + versionCode + ", Version Name: " + versionName + ")");
+                i.putExtra(Intent.EXTRA_TEXT   , "");
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(AppInfoActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
