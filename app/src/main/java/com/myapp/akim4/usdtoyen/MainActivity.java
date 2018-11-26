eb475e72d4074afb729610f2478b03b8b86e1f47
Adam Kim <akim456@gmail.com>
2018-11-26 04:11:42 -0500

    removed rating from mainact, swapped order of dev buttons and added color on the info activity page

--- app/src/main/java/com/myapp/akim4/usdtoyen/MainActivity.java
+++ app/src/main/java/com/myapp/akim4/usdtoyen/MainActivity.java
@@ -407,24 +407,24 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-                //RATE APP BUTTON
-                Button rateAppButton = (Button) findViewById(R.id.rateAppButton);
-                rateAppButton.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        Uri uri = Uri.parse("market://details?id=" + getPackageName());
-                        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-                        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
-                                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
-                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                        try {
-                            startActivity(goToMarket);
-                        } catch (ActivityNotFoundException e) {
-                            startActivity(new Intent(Intent.ACTION_VIEW,
-                                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
-                        }
-                    }
-                });
+//                //RATE APP BUTTON
+//                Button rateAppButton = (Button) findViewById(R.id.rateAppButton);
+//                rateAppButton.setOnClickListener(new View.OnClickListener(){
+//                    @Override
+//                    public void onClick(View v){
+//                        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+//                        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+//                        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+//                                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+//                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+//                        try {
+//                            startActivity(goToMarket);
+//                        } catch (ActivityNotFoundException e) {
+//                            startActivity(new Intent(Intent.ACTION_VIEW,
+//                                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+//                        }
+//                    }
+//                });
 
                 //SHARE APP BUTTON
                 Button shareAppButton = (Button) findViewById(R.id.shareAppButton);
